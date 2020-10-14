f2ab4b12631d17b23622c35e47e8f4fc3fba6588
Halvor Sogn Haug <halvorsh@gmail.com>
2020-10-14 20:08:25 +0200

    Some improvements to rewards, more work will still be needed

--- SquigglesEnvironment.py
+++ SquigglesEnvironment.py
@@ -23,8 +23,7 @@ class SquigglesEnvironment(py_environment.PyEnvironment):
         self._observation_spec = array_spec.BoundedArraySpec(
             shape=(2,), dtype=np.int32, minimum=0, name='observation')
         self._state = 0
-        self._time_since_real_play = 0
-        self._time_since_second_real_play = 0
+        self._time_since_real_plays = np.array([0 for _ in range(num_notes_out)]).astype(np.int32)
         self._episode_ended = False
         self._time_between_squiggles_beats = 60*100//4*bpm
         self._time_since_last_play = 0
@@ -43,10 +42,15 @@ class SquigglesEnvironment(py_environment.PyEnvironment):
 
     def _reset(self):
         self._state = 0
-        self._time_since_real_play = 0
-        self._time_since_second_real_play = 0
         self._episode_ended = False
-        return ts.restart(np.array([self._time_since_real_play, self._time_since_second_real_play], dtype=np.int32))
+        self._time_since_real_plays = np.zeros(self._time_since_real_plays.shape).astype(np.int32)
+        self._number_of_plays = 0
+        self._number_of_real_plays = 0
+        self._squiggles_list = [Squiggles() for i in range(len(self._squiggles_list))]
+        self._squiggles_input = [0 for i in range(16)]
+        for squig in self._squiggles_list:
+            squig.update_o()
+        return ts.restart(self._time_since_real_plays)
 
     def _step(self, action):
         if self._episode_ended:
@@ -54,8 +58,7 @@ class SquigglesEnvironment(py_environment.PyEnvironment):
 
         if action == 1 or action == 0:
             self._state += 1
-            self._time_since_second_real_play += 1
-            self._time_since_real_play += 1
+            self._time_since_real_plays += 1
             self._time_since_last_play += 1
         else:
             raise ValueError('`action` should be 0 or 1.')
@@ -72,30 +75,31 @@ class SquigglesEnvironment(py_environment.PyEnvironment):
         play = False
         if self._state%self._time_between_squiggles_beats == 0 and self._squiggles_list[-1].o[(self.state/self._time_between_squiggles_beats)%16] == 1:
             play = True
-            self._time_since_second_real_play = self._time_since_real_play
-            self._time_since_real_play = 0
+            self._time_since_real_plays = np.roll(self._time_since_real_plays,1)
+            self._time_since_real_plays[0] = 0
+
 
         output = 1 if play else 0
         self._number_of_real_plays += output
 
         if self._state >= 1000:
             #50 is a random constant here, should probably be tweaked
-            reward = -np.absolute(self._number_of_plays - self._number_of_real_plays)*50
-            return ts.termination(np.array([self._time_since_real_play, self._time_since_second_real_play], dtype=np.int32), reward)
+            reward = -np.absolute(self._number_of_plays - self._number_of_real_plays)*10
+            return ts.termination(self._time_since_real_plays, reward)
         else:
             reward = 0
 
             if action == 1:
                 self._number_of_plays += 1
                 if self._time_since_last_play < self._time_between_squiggles_beats/3:
-                    reward = -100 #Random number, probably needs tweaking
+                    reward = -10 #Random number, probably needs tweaking
                 else:
                     current_closeness_to_real_beat = self._state%self._time_between_squiggles_beats
                     if current_closeness_to_real_beat > self._time_between_squiggles_beats/2:
                         current_closeness_to_real_beat -= self._time_between_squiggles_beats
-                    reward = -np.absolute(0 - current_closeness_to_real_beat)
+                    reward = 10-np.absolute(0 - current_closeness_to_real_beat)
 
                 self._time_since_last_play = 0
 
             return ts.transition(
-                np.array([self._time_since_real_play, self._time_since_second_real_play], dtype=np.int32), reward=reward, discount=1.0)
+                self._time_since_real_plays, reward=reward, discount=1.0)
