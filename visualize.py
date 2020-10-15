b63fbeb16c0313c1d68c9f2a527ea0a792650edf
Mia Kvalsund <mkk>
2020-10-15 20:39:56 +0200

    changing the loop

--- visualize.py
+++ visualize.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 from SquigglesEnvironment import SquigglesEnvironment
 
@@ -6,12 +7,16 @@ env = SquigglesEnvironment()
 state = env.reset()
 print(state)
 
-time = []
-beats = []
-for _ in range(10000):
-    state = env.step(1)
-    time.append(state.observation[0])
-    beats.append(state.observation[1])
+N = env.observation_spec().shape[0]
+ITER = 10000
 
-plt.plot(time, beats)
+beats = [[] for _ in range(N)]
+for _ in range(ITER):
+    state = env.step(1)
+    for i in range(N):
+        beats[i].append(state.observation[i])
+
+times = np.arange(0,ITER,1)
+for i in range(N):
+    plt.plot(times, beats[i])
 plt.show()
