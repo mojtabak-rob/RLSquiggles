a645a41216623e8da758ffa2efc7762312d4e41b
Mia Kvalsund <mkk>
2020-10-15 20:56:35 +0200

    Added a plot showing the beats

--- visualize.py
+++ visualize.py
@@ -8,15 +8,27 @@ state = env.reset()
 print(state)
 
 N = env.observation_spec().shape[0]
-ITER = 10000
+ITER = 1000
 
 beats = [[] for _ in range(N)]
 for _ in range(ITER):
-    state = env.step(1)
+    state = env.step(0)
     for i in range(N):
         beats[i].append(state.observation[i])
 
 times = np.arange(0,ITER,1)
 for i in range(N):
     plt.plot(times, beats[i])
+
+plt.figure()
+the_hits = np.zeros(ITER)
+
+for i in range(ITER):
+    play = False
+    for j in range(N):
+        if beats[j][i] == 0:
+            play = True
+    the_hits[i] = int(play)
+
+plt.plot(times, the_hits)
 plt.show()
