d02df0094f0e1b1c11091e97b544ab2d1538d94f
mia-katrin <mia.kvalsund@gmail.com>
2021-01-20 14:46:47 +0100

    Cleaning

---
+++ versions/version_trigger_happy/Squiggles.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+class Squiggles():
+    """Squiggles simulation based on Mike's environment"""
+    def __init__(self):
+        self.hist = [np.random.rand() for i in range(16)]
+        self.o = [0 for i in range(16)]
+        self.o_prev = [0 for i in range(16)]
+        self.d = 0.9 #Discount
+        self.i = 0   #Inverse
+        self.k = 0   #Exponent
+        self.ham = -1
+
+    def print_state(self):
+        print(self.hist)
+        print(self.o)
+
+    """def f(self, x, exponent, is_inverse):
+        is_inverse = -1 if is_inverse else 0
+
+
+        nonliniarity = np.abs(2*x-1)
+        nonliniarity = nonliniarity**exponent
+        nonliniarity *= is_inverse
+        if x < 0.5:
+            nonliniarity *= -1
+        nonliniarity = nonliniarity/2
+
+        return nonliniarity
+
+    def determancy(self):
+        result = 0
+        minimax = 2
+        maximin = -1
+
+        for i in range(16):
+            if self.hist[i] >= 0.5:
+                if self.hist[i] < minimax:
+                    minimax = self.hist[i]
+            else:
+                if self.hist[i] > maximin:
+                    maximin = self.hist[i]
+
+        if minimax == 2 and maximin == -1:
+            result = 0
+        else:
+            result = np.abs(self.f(minimax, self.k, self.i) - self.f(maximin, self.k, self.i))
+
+        return result"""
+
+    def update_h(self, listening_to):
+        for i in range(16):
+            self.hist[i] = self.hist[i]*self.d + listening_to[i]*(1-self.d)
+
+    def update_o(self):
+        self.ham = 0
+        for i in range(16):
+            self.o_prev[i] = self.o[i]
+            test = np.random.rand()
+            self.o[i] = 1 if self.hist[i] > test else 0 #self.f(self.hist[i], self.k, self.i) > test else 0
+            self.ham += 1 if self.o_prev[i] == self.o[i] else 0
+
+        self.ham /= 16
---
+++ versions/version_trigger_happy2/cart_pole.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 12:40:41 2020
+"""
+
+# Module imports
+import tensorflow as tf
+from tf_agents.environments import tf_py_environment
+from tf_agents.policies.random_tf_policy import RandomTFPolicy
+from tf_agents.replay_buffers.tf_uniform_replay_buffer import TFUniformReplayBuffer
+from tf_agents.trajectories import trajectory
+from tf_agents.policies.policy_saver import PolicySaver
+
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import numpy as np
+
+# Written by us imports
+from SquigglesEnvironment2 import SquigglesEnvironment
+from experience_replay import ExperienceReplay
+from basic_agent import generic_dqn_agent # a function
+
+from tf_agents.environments import suite_gym
+
+# Globals
+NUMBER_ITERATION = 20000
+COLLECTION_STEPS = 1
+BATCH_SIZE = 64
+EVAL_EPISODES = 10
+EVAL_INTERVAL = 1000
+SHOW_INTERVAL = EVAL_INTERVAL*4
+
+def get_average_return(environment, policy, episodes=10):
+
+    total_return = 0.0
+
+    for _ in range(episodes):
+        time_step = environment.reset()
+        episode_return = 0.0
+
+        while not time_step.is_last():
+            action_step = policy.action(time_step)
+            time_step = environment.step(action_step.action)
+            episode_return += time_step.reward
+
+        total_return += episode_return
+    avg_return = total_return / episodes
+
+    return avg_return.numpy()[0]
+
+def init():
+    train_env = SquigglesEnvironment()
+    evaluation_env = SquigglesEnvironment()
+
+    train_env = tf_py_environment.TFPyEnvironment(train_env)
+    evaluation_env = tf_py_environment.TFPyEnvironment(evaluation_env)
+
+    agent, _ = generic_dqn_agent(train_env)
+
+    experience_replay = ExperienceReplay(agent, train_env, BATCH_SIZE)
+
+    return agent, train_env, evaluation_env, experience_replay
+
+def training_loop(agent, train_env, evaluation_env, experience_replay):
+    agent.train_step_counter.assign(0)
+
+    avg_return = get_average_return(evaluation_env, agent.policy, EVAL_EPISODES)
+    returns = [avg_return]
+
+    for _ in tqdm(range(NUMBER_ITERATION)):
+
+        for _ in range(COLLECTION_STEPS):
+            experience_replay.timestamp_data(train_env, agent.collect_policy)
+
+        experience, info = next(experience_replay.iterator)
+        train_loss = agent.train(experience).loss
+
+        if agent.train_step_counter.numpy() % EVAL_INTERVAL == 0:
+            avg_return = get_average_return(evaluation_env, agent.policy, EVAL_EPISODES)
+            print('Iteration {0} – Average Return = {1}, Loss = {2}.'.format(agent.train_step_counter.numpy(), avg_return, train_loss))
+            returns.append(avg_return)
+
+            if agent.train_step_counter.numpy() % SHOW_INTERVAL == 0:
+                show_current(1000, evaluation_env, agent.policy)
+
+    return returns
+
+def show_current(ITER, env, policy):
+    N = env.observation_spec().shape[0]
+    state = env.reset()
+
+    the_hits = np.zeros(ITER)
+    agent_hits = []
+    rewards = []
+    for j in range(ITER):
+        a = policy.action(state)
+        agent_hits.append(a.action)
+
+        state = env.step(a)
+        rewards.append(state.reward)
+
+        play = state.observation[0][0] == 0
+        the_hits[j] = int(play)
+
+    plt.figure()
+    plt.plot(the_hits)
+    plt.plot(agent_hits)
+    plt.title("Action and space")
+
+    plt.figure()
+    plt.plot(rewards)
+    plt.title("Rewards")
+    plt.show()
+
+def main():
+    agent, train_env, evaluation_env, experience_replay = init()
+
+    returns = training_loop(
+        agent,
+        train_env,
+        evaluation_env,
+        experience_replay
+    )
+
+    # save policy
+    PolicySaver(agent.policy).save('policy_saved')
+
+    plt.plot(returns)
+    plt.title("Rewards overall")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
