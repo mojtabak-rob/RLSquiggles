51ebef94adedbe66b940d5251558e64fcd325238
mia-katrin <mia.kvalsund@gmail.com>
2021-02-10 14:37:12 +0100

    Some minor changes

--- environment_testing.py
+++ environment_testing.py
@@ -1,3 +1,5 @@
+""" Code to make a 2D and 3D plot, as well as print some stats, of the current
+SquigglesEnvironment """
 import tensorflow as tf
 from tf_agents.environments import tf_py_environment
 
@@ -5,7 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import wavio
 
-from versions.mirror_no_silence_punish.SquigglesEnvironment import SquigglesEnvironment
+from env.SquigglesEnvironment import SquigglesEnvironment
 
 ## A way to label mirror_no_silence_punish's environment observations
 # OBS: 2 versions:
@@ -17,14 +19,15 @@ def label(observation):
 
     # if observation[0] == sixteenth:
     if observation[0]%sixteenth == 0:
+
         return 1
     return 0
 
-env = SquigglesEnvironment()
+env = SquigglesEnvironment(num_notes=2) # Code does not support more than 2 notes
 env = tf_py_environment.TFPyEnvironment(env)
 
 N = env.observation_spec().shape[0]-1 # Last observation is action, does not change labelling
-ITER = 10000
+ITER = 30000
 
 obs = [[],[]] # One list for each action
 
@@ -47,6 +50,10 @@ for _ in range(ITER):
 zero = np.transpose(obs[0])
 ones = np.transpose(obs[1])
 
+print("\nStats:")
+print("Number of zeros:", len(obs[0]), ",", len(obs[0])*100/ITER, "%")
+print("Number of ones:", len(obs[1]), ",", len(obs[1])*100/ITER, "%")
+
 # 2D plot
 fig = plt.figure()
 plt.scatter(zero[0], zero[1], marker="o", label="Right answer is 0")
@@ -54,7 +61,6 @@ plt.scatter(ones[0], ones[1], marker="x", label="Right answer is 1")
 plt.legend()
 plt.xlabel('First counter')
 plt.ylabel('Second counter')
-plt.show()
 
 # 3D plot
 fig = plt.figure()
